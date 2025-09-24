451e30d5af572a89c6038b8e4897eee6fd09711f
Edward Matrix (Lê Hiệp) <98216285+hiep1312@users.noreply.github.com>
2025-09-24 13:26:18 +0700

    Delete Bai1/tempCodeRunnerFile.php

--- Bai1/tempCodeRunnerFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arrUnique = array_unique($arr);
-$arrSum = array_sum($arr);
-$arrReverse = array_reverse($arr);
-
-echo "Mảng sau khi xóa trùng lặp: [" . implode(", ", $arrUnique) . "]" . PHP_EOL;
-echo "Tính tổng mảng: " . $arrSum . PHP_EOL;
-echo "Mảng sau khi đảo ngược: [" . implode(", ", $arrReverse). "]" . PHP_EOL;
