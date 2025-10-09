b472e968683346b7ac3342efaac8f70f212dd099
Lê Danh Hiệp <ledanhhiep2005@gmail.com>
2025-10-09 13:11:31 +0700

    Update count

--- EcommerceProject/database/seeders/ProductSeeder.php
+++ EcommerceProject/database/seeders/ProductSeeder.php
@@ -21,7 +21,7 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         /* Create random 25 products with images and categories */
-        $products = Product::factory(25)->has(
+        $products = Product::factory(23)->has(
             Image::factory(5)->product()->state(new Sequence(
                 fn (Sequence $sequence) => [
                     'is_main' => $sequence->index === 0,
